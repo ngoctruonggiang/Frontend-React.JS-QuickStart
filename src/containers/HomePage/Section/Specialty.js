85107edf28cf5221ab81ac36d5ab7ec3f0a64f46
ngoctruonggiang <trggg2004@gmail.com>
2026-03-30 20:41:34 +0700

    Customize Tùy Chỉnh React-Slider-Carouse

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -5,6 +5,8 @@ import "./Specialty.scss";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";//import css cua thu vien slick
+import shogun from "../../../assets/specialty/Raiden.jpg";
+import tuyet from "../../../assets/specialty/TuyettBangDe2.jpg";
 
 class Specialty extends Component {
 
@@ -15,41 +17,52 @@ class Specialty extends Component {
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 1,//so luong slide chuyen khi click
     };
     return (
      <>
      <div className = "section-specialty">
-        <div className = "specialty-content">
+        <div className = "specialty-container">
             <div className = "specialty-header">
-                <span className = "title-specialty">Chuyên khoa</span>
-                <button className = "btn-specialty">Xem thêm</button>
+                <span className = "title-section">Chuyên khoa</span>
+                <button className = "btn-section">Xem thêm</button>
             </div>
             <div className="specialty-body">
                 <Slider {...settings}>
                     <div className="specialty-customize">
-                        <div className="bg-image">Hình 1</div>
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${shogun})`}}></div>
                         <div>Cơ xương khớp 1</div>
                     </div>
                     <div className="specialty-customize">
-                        <div className="bg-image">Hình 2</div>
-                        <div>Cơ xương khớp 2</div>
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${tuyet})`}}></div>
+                        <div>Cơ xương khớp 1</div>
                     </div>
                     <div className="specialty-customize">
-                        <div className="bg-image">Hình 3</div>
-                        <div>Cơ xương khớp 3</div>
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${tuyet})`}}></div>
+                        <div>Cơ xương khớp 1</div>
                     </div>
                     <div className="specialty-customize">
-                        <div className="bg-image">Hình 4</div>
-                        <div>Cơ xương khớp 4</div>
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${shogun})`}}></div>
+                        <div>Cơ xương khớp 1</div>
                     </div>
                     <div className="specialty-customize">
-                        <div className="bg-image">Hình 5</div>
-                        <div>Cơ xương khớp 5</div>
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${tuyet})`}}></div>
+                        <div>Cơ xương khớp 1</div>
                     </div>
                     <div className="specialty-customize">
-                        <div className="bg-image">Hình 6</div>
-                        <div>Cơ xương khớp 6</div>
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${shogun})`}}></div>
+                        <div>Cơ xương khớp 1</div>
+                    </div>
+                    <div className="specialty-customize">
+                        <div className="bg-image" 
+                        style={{backgroundImage: `url(${tuyet})`}}></div>
+                        <div>Cơ xương khớp 1</div>
                     </div>
                 </Slider>
             </div>
